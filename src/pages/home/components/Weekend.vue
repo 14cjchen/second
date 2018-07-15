90477021e71335b18145a06c78285c99e6ee9971
14cjchen <812398915@qq.com>
2018-07-15 08:55:02 +0800

    addPartOfRecommend

---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <div class="title">周末去哪</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img"
+               :src="item.imgUrl"/>
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomerWeekend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/bc/7aefc32e7f855a.jpg_r_640x214_f88989f3.jpg',
+        title: '汕头必游TOP5',
+        desc: '“海滨邹鲁，美食之乡'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1511/f7/90c1c5f6f3dd59.jpg_r_640x214_3cb4974e.jpg',
+        title: '河源必游TOP10',
+        desc: '走进客家古邑，感受万绿河源'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/4a/ac007012d54bd4.jpg_r_640x214_eefa6ade.jpg',
+        title: '江门必游TOP10',
+        desc: '你不可错过的休闲度假之地'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/mixins.styl"
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item-img-wrapper
+    overflow: hidden
+    height: 0
+    padding-bottom: 33.9%
+   .item-img
+     width: 100%
+     padding: .1rem
+  .item-info
+    padding: .1rem
+  .item-title
+    line-height: .54rem
+    font-size: .32rem
+    ellipsis()
+  .item-desc
+    line-height: .4rem
+    color: #333
+    ellipsis()
+
+</style>
